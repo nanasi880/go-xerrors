21db8917d9e9fc4841b165ec9fb74ce46d68f9c9
nanasi880 <nanasi880@gmail.com>
2019-04-21 16:10:29 +0900

    add godoc

--- doc.go
+++ doc.go
@@ -5,6 +5,59 @@
 // This package difference to "golang.org/x/errors" package is error wrapping style.
 // Original package is using printf style ( e.g. ": %w" ) however this package using Wrap function.
 //
-// Example:
-//     newErr := xerrors.New("Oops").Wrap(baseErr).Build()
+// Simply Example:
+//     package main
+//
+//     import (
+//	       "fmt"
+//
+//	       "go.nanasi880.dev/xerrors"
+//     )
+//
+//     func main() {
+//	       err := newError()
+//
+//	       fmt.Printf("%v\n", err)
+//	       // Output:
+//	       // fail
+//
+//	       fmt.Printf("%+v\n", err)
+//	       // Output:
+//	       // fail
+//	       //     mypackage/main.newError
+//	       //         /Users/myname/project/main.go:23
+//     }
+//
+//     func newError() error {
+//	       return xerrors.New("fail").Build()
+//     }
+//
+//
+// Helper Example:
+//     package main
+//
+//     import (
+//          "fmt"
+//
+//           "go.nanasi880.dev/xerrors"
+//      )
+//
+//      func main() {
+//           err := newError()
+//
+//           fmt.Printf("%v\n", err)
+//           // Output:
+//           // fail
+//
+//           fmt.Printf("%+v\n", err)
+//           // Output:
+//           // fail
+//           //     mypackage/main.main
+//           //         /Users/myname/project/main.go:10
+//      }
+//
+//      func newError() error {
+//           xerrors.Helper() // this function is Helper. do not logging stacktrace.
+//           return xerrors.New("fail").Build()
+//      }
 package xerrors // import "go.nanasi880.dev/xerrors"
